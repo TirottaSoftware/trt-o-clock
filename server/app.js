464df84358299cf66f0a67364377cc7bd485d31d
Cristian Tirotta <59051220+TirottaSoftware@users.noreply.github.com>
2022-06-25 11:31:13 +0300

    Initialised GraphQL API into the backend

--- server/app.js
+++ server/app.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const { ApolloServer } = require("apollo-server")
+const { typeDefs } = require('./schema/typeDefs')
+const { resolvers } = require('./schema/resolvers')
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+
+apolloServer.listen(4000).then(({ url }) => {
+  console.log("Apollo Server running on " + url)
+})
 
 app.use(cors());
 app.use(express.json());
