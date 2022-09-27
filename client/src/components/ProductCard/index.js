2e815a6f8d82da9cc328a4b22414e5aaa3cfc086
Cristian Tirotta <59051220+TirottaSoftware@users.noreply.github.com>
2022-09-27 22:42:05 +0300

    Mobile Responsiveness for Product Page

--- client/src/components/ProductCard/index.js
+++ client/src/components/ProductCard/index.js
@@ -1,4 +1,5 @@
 import './ProductCard.css'
+import { Link } from 'react-router-dom'
 
 const ProductCard = ({ watch }) => {
     return (
@@ -6,7 +7,9 @@ const ProductCard = ({ watch }) => {
             <img src={watch.imageUrl} alt={`img-${watch.model}`} />
             <h1>{watch.model}</h1>
             <h2>${watch.price}</h2>
-            <button className="btn-cta">View</button>
+            <Link className="product-cta" to={`/product/${watch.id}`}>
+                <button className="btn-cta">View</button>
+            </Link>
         </div>
     )
 }
