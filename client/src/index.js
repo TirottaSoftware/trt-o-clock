29867d73a1c3cb3dcedc59fbcbba950f3c7b5034
Cristian Tirotta <59051220+TirottaSoftware@users.noreply.github.com>
2022-09-27 22:29:45 +0300

    Added Scrolling on Page Change

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import Home from "./pages/Home";
 import Store from "./pages/Store";
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import ProductPage from "./pages/Product";
+import ScrollToTop from "./components/ScrollToTop";
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
@@ -15,6 +16,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ApolloProvider client={client}>
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
